3093d5a676d484e78259020329f74bba5290615e
Wiilames <willmiranda.94@gmail.com>
2021-08-19 20:08:09 -0400

    Implementando o metdodo do clique nas imagens "pedra" "papel" "tesoura"

--- app/src/main/java/com/willzinho/pedra/MainActivity.java
+++ app/src/main/java/com/willzinho/pedra/MainActivity.java
@@ -3,6 +3,9 @@ package com.willzinho.pedra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void selecionadoPedra(View view){
+
+        this.opcaoSelecionada("pedra");
+    }
+    public void selecionadoPapel(View view){
+
+        this.opcaoSelecionada("papel");
+    }
+    public void selecionadoTesoura(View view){
+
+        this.opcaoSelecionada("tesoura");
+    }
+
+
+    public void opcaoSelecionada(String opcaoSelecionada ){
+        System.out.println("Item clicado " + opcaoSelecionada );
+
+        int numero = new Random().nextInt(3);
+
+
+
+    }
+
+
 }
